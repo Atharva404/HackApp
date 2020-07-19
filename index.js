9a22f48e22e4fedaaf2d5f8c81f4e9700087b1bf
Atharva Gupta <atharva.gu@gmail.com>
2020-07-19 15:11:11 -0500

    ch

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function login() {
- var userEmail = document.getElementById("email_field").value;
- var userPass = document.getElementById("password_field").value;
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
 
- window.alert(userEmail + " " + userPass);
- 
+      document.getElementById("user_div").style.display = "initial";
+      document.getElementById("login_div").style.display = "none";
+    } else {
+      // No user is signed in.
+
+      document.getElementById("user_div").style.display = "none";
+
+      document.getElementById("login_div").style.display = "initial";
+    }
+  });
+function login() {
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    alert(userEmail + " " + userPass);
 }
